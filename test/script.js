a309efff6a0ad29e9f7c6da24e8662d90861e356
Catsurcool <49851503+Catsurcool@users.noreply.github.com>
2020-03-10 21:06:13 -0700

    er

--- test/script.js
+++ test/script.js
@@ -23,6 +23,7 @@ function toE() {
   );
   // dFP = '<textarea id="englishText" class="input" style="width:99%; font-family:Palatino Linotype, Book Antiqua, Palatino, serif;" maxlength="500" cols="30" rows="10" value="">' + engStr + '</textarea>'
   document.getElementById('ta1space').innerHTML = '<textarea rows="8" cols="80" id="ta1" class="dragon" placeholder="Text to translate into English" style="margin-top: -20px;" spellcheck="false" value="">' + engStr + '</textarea>';
+  engStr = ''
   // console.log(engStr)
 }
 // ree ree2 ree3
