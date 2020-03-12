4b576aae243c186b98942ae07738c137c3c518a6
Catsurcool <49851503+Catsurcool@users.noreply.github.com>
2020-03-11 18:06:05 -0700

    Changed some stuff

--- test/script.js
+++ test/script.js
@@ -16,16 +16,15 @@ var dovahWords = [
 function toE() {
   var words = ta2.value.split(' ');
   console.log(words);
-  words.forEach(
-    function (element) {
-      console.log(element);
-      if (dovahWords.includes(element)) {
-        engStr += element;
-      } else {
-        engStr += '?';
-      }
+  words.forEach(function (element) {
+    console.log(element);
+
+    if (dovahWords.includes(element)) {
+      engStr += element;
+    } else {
+      engStr += element;
     }
-  );
+  });
   // dFP = '<textarea id="englishText" class="input" style="width:99%; font-family:Palatino Linotype, Book Antiqua, Palatino, serif;" maxlength="500" cols="30" rows="10" value="">' + engStr + '</textarea>'
   document.getElementById('ta1space').innerHTML = '<textarea rows="8" cols="80" id="ta1" class="dragon" placeholder="Text to translate into English" style="margin-top: -20px;" spellcheck="false" value="">' + engStr + '</textarea>';
   engStr = ''
